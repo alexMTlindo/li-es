d50e40ad8832798bef1b1ced586c4ac10c1addbe
alexMTlindo <143510111+alexMTlindo@users.noreply.github.com>
2023-09-14 09:09:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var idade
+var n
+var contador=0
+var media=0
+
+for(n=1;n<=10;n++){
+    idade = parseInt(prompt("Digite um número: ") )
+    if(idade>=18){
+        contador++
+    }
+}
+ alert ("  :" + contador)
+
